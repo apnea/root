c9543d6f44537a801e431cfecd975fb487be288a
apnea <apnea@users.noreply.github.com>
2015-03-10 10:23:22 +0000

    update form name

--- javascripts/submit.js
+++ javascripts/submit.js
@@ -1,7 +1,7 @@
 
 function OnButton1()
 {
-document.Form1.action = "response1.php"
+document.mailform.action = "response1.php"
 action="//rangshifter.us10.list-manage.com/subscribe/post?u=2c1168ea43f4ed7f3e516a472&amp;id=4f3fc6d76d"
 method="post"
 id="mc-embedded-subscribe-form"
@@ -9,14 +9,14 @@ name="mc-embedded-subscribe-form"
 class="validate"
 target="_blank"
 novalidate
-document.Form1.submit(); // Submit the page
+document.mailform.submit(); // Submit the page
 return true;
 }
 
 function OnButton2()
 {
-document.Form1.action = "response2.php"
-document.Form1.target = "_blank"; // Open in a new window
-document.Form1.submit(); // Submit the page
+document.mailform.action = "response2.php"
+document.mailform.target = "_blank"; // Open in a new window
+document.mailform.submit(); // Submit the page
 return true;
 }
